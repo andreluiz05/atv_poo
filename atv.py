8cde7fc50f07233a48b38ed03a31965f130bc36f
andreluiz05 <andreluizdesantanasilva123@gmail.com>
2025-11-13 21:59:39 -0300

    atv.py

--- atv.py
+++ atv.py
@@ -9,29 +9,12 @@ while user == "" or user.isnumeric():
 print("Usuário aceito!")
 
 idade = input("Digite sua idade: ")
-try:
-    idade = int(idade)
 
-    if idade <= 0:
-        print("Erro! A idade deve ser maior que zero!")
+while idade == "" or not idade.isnumeric() or int(idade) <= 0:
+    print("Erro! Idade inválida! Digite apenas números e maior que zero.")
+    idade = input("Digite sua idade corretamente: ")
 
-        while idade <= 0 or idade == "":
-            idade = input("Digite sua idade corretamente: ")
+idade = int(idade)
 
-            if idade == "":
-                print("Erro! Não deixe vazio!")
-                
-
-            idade = int(idade)
-
-    print("Idade aceita!")
-
-except:
-    print("Erro! Você digitou número inválido ou não inseriu idade!")
-
-    while idade == "" or not idade.isnumeric() or int(idade) <= 0:
-        idade = input("Digite sua idade corretamente: ")
-
-    idade = int(idade)
-
-print("Seja bem vindo,", user,"! ""Você tem", idade,"anos !", " CADASTRO CONCLUÍDO COM SUCESSO!")
+print("Idade aceita!")
+print("Seja bem vindo,", user,"! Você tem", idade,"anos! CADASTRO CONCLUÍDO COM SUCESSO!")
